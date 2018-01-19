ab870488bc53f6487f78bb4393f9443b4ce47c55
tobyjaguar <talgya@gmail.com>
2018-01-19 15:23:35 -0800

    failed to hide the secrets
    I tired to keep the constructor internal, but this ends with the same compile error:
    "This contract does not implement all functions and thus cannot be created."
    hmm

--- Remittance.sol
+++ Remittance.sol
@@ -11,42 +11,51 @@ contract Remittance {
 
     //Constructor
     function Remittance(bytes32 bobPass, bytes32 carolPass)
-        public {
+        internal {
             owner = msg.sender;
             bobHashedPass = keccak256(bobPass);
             carolHashedPass = keccak256(carolPass);
 
         }
+        
+        function varPass() 
+        public
+        view
+        returns (bytes32 bobHashed, bytes32 carolHashed) {
+            bobHashed = bobHashedPass;
+            carolHashed = carolHashedPass;
+            return (bobHashed, carolHashed);
+        }
+}
 
-    //get contract balance
-    function getBalance()
-    public
-    constant
-    returns (bool success, uint256 balance) {
-        return (true, this.balance);
-    }
+contract remitFunds is Remittance {
 
-    //receive funds
-    function sendFunds()
+    bytes32 bobHashed;
+    bytes32 carolHashed;
+    //bytes32 pass1;
+    //bytes32 pass2;
+    function getVars() 
     public
-    payable
-    returns (bool success) {
-        require(msg.value > 0);
-        return (true);
+    view
+    returns (bytes32, bytes32) {
+        return super.varPass();
     }
 
     //function to send contract balance
     //must verify both passwords
-    function remitFunds(bytes32 pass1, bytes32 pass2)
+    function withdrawFunds(bytes32 pass1, bytes32 pass2)
         public
         returns (bool success) {
             require(this.balance > 0);
-            //check/sanitize user data
-            if (bobHashedPass == keccak256(pass1)
-            && carolHashedPass == keccak256(pass2)) {
+            //check-sanitize user data
+            
+            if (bobHashed == keccak256(pass1)
+            && carolHashed == keccak256(pass2)) {
                 msg.sender.transfer(this.balance);
                 return true;
             }
         }
+
+}
 
 }
