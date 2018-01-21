41510dca60c594485db9e800daa8b14a4d0df77a
tobyjaguar <talgya@gmail.com>
2018-01-20 17:59:43 -0800

    changed withdrawFunds
    need to make sure that the supplied pre-hashed password would hash to the set hashed passwords.

--- Remittance2.sol
+++ Remittance2.sol
@@ -1,45 +1,46 @@
 pragma solidity ^0.4.13;
 
+
 contract Remittance {
-    
-    address owner;
-    bytes32 bobHashedPass;
-    bytes32 carolHashedPass;
-    
+
+    address public owner;
+    bytes32 public bobHashedPass;
+    bytes32 public carolHashedPass;
+
     function Remittance()
-    public 
+    public
     {
-    
-     owner = msg.sender;
-     
+
+        owner = msg.sender;
+
     }
 
-    function setPass(bytes32 hashedPass1, bytes32 hashedPass2) 
-    public 
+    function setPass(bytes32 hashedPass1, bytes32 hashedPass2)
+    public
     {
         require(owner == msg.sender);
         bobHashedPass = hashedPass1;
         carolHashedPass = hashedPass2;
     }
-    
-    function receiveFunds() 
+
+    function receiveFunds()
     public
     payable
     returns (bool success)
     {
         require(msg.sender == owner);
         return true;
-        
+
     }
-    
+
     function withdrawFunds(bytes32 pass1, bytes32 pass2)
-    public 
+    public
     {
-        if(pass1 == bobHashedPass || pass1 == carolHashedPass) {
-            if(pass2 == bobHashedPass || pass2 == carolHashedPass) {
+        if (bobHashedPass == keccak256(pass1) || carolHashedPass == keccak256(pass1)) {
+            if (bobHashedPass == keccak256(pass2) || carolHashedPass == keccak256(pass2)) {
                 msg.sender.transfer(this.balance);
             }
         }
     }
-    
+
 }
