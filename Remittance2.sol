09d3507bff8c7af8a0406a9034d51020164e3db5
Toby Jaguar <talgya@gmail.com>
2018-01-24 12:29:51 -0800

    updated remit and test

--- Remittance2.sol
+++ Remittance2.sol
@@ -7,8 +7,8 @@ contract Remittance {
     bytes32 public bobHashedPass;
     bytes32 public carolHashedPass;
 
-    address remitAddy;
-    bool unlocked;
+    address public remitAddy;
+    bool public unlocked;
     bytes32 public testPass1;
     bytes32 public testPass2;
 
@@ -18,43 +18,57 @@ contract Remittance {
         owner = msg.sender;
     }
 
+    function getBalance()
+    public
+    constant
+    returns (uint256 _balance)
+    {
+        return this.balance;
+    }
+
     function setPass(bytes32 hashedPass1, bytes32 hashedPass2)
     public
     payable
     returns (bool success)
     {
         require(owner == msg.sender);
-	require(hashedPass1 != 0);
-	require(hashedPass2 != 0);
+        require(hashedPass1 != 0);
+        require(hashedPass2 != 0);
         bobHashedPass = hashedPass1;
         carolHashedPass = hashedPass2;
-	return true;
-    }
-
-    function checkPass()
-    {
-        require(bobHashedPass == testPass1);
-	require(carolHashedPass == testPass2);
-	unlocked = true;
-    }
-
-    function checkKey(bytes32 pass1, bytes32 pass2)
-    {
-	testPass1 = keccak256(pass1);
-	testPass2 = keccak256(pass2);
-	remitAddy = msg.sender;
-	checkPass();
+        return true;
     }
 
     function withdrawFunds(bytes32 pass1, bytes32 pass2)
     public
     returns(bool success)
     {
-	checkKey(pass1, pass2);
-	require(this.balance > 0);
-	require(remitAddy != 0);
+        require(this.balance > 0);
+        require(remitAddy != 0);
+        checkKey(pass1, pass2);
         remitAddy.transfer(this.balance);
-	return true;
+        return true;
+    }
+
+    function checkPass()
+    internal
+    returns (bool success)
+    {
+        require(bobHashedPass == testPass1);
+        require(carolHashedPass == testPass2);
+        unlocked = true;
+        return true;
+    }
+
+    function checkKey(bytes32 pass1, bytes32 pass2)
+    internal
+    returns (bool success)
+    {
+        testPass1 = keccak256(pass1);
+        testPass2 = keccak256(pass2);
+        remitAddy = msg.sender;
+        checkPass();
+        return true;
     }
 
 }
--- remittance.js
+++ remittance.js
@@ -3,18 +3,23 @@ var Remittance = artifacts.require("./Remittance.sol");
 contract ('Remittance', function(accounts) {
 
   var owner = accounts[0];
-  var contractInitBalance;
+  var remitAddress = accounts[1];
   var amount = 1000;
-  var pass1 = "abc";
+
+  //var pass1 = "0x616263";
+  //var pass2 = "0x646566"
+  //var hashedPass1 = "0x9b8075e3114a237714bcee811cbb0337de6d1423cb2947266772aae5963ec8e5";
+  //var hashedPass2 = "0xc6f91c219c527df164f349f01610e87533ec0c3b5c11fddd8052d101332c0257";
+  //Hash here
+  var pass1 = "acb";
   var pass2 = "def";
+  var hashedPass1 = web3.sha3(pass1);
+  var hashedPass2 = web3.sha3(pass2);
 
   beforeEach(function() {
-    return Remittance.new(pass1, pass2, {from: owner})
+    return Remittance.new({from: owner})
     .then(function(instance) {
       contractInstance = instance;
-      web3.eth.sendTransaction({from: owner, to: contractInstance.address,
-        value: amount});
-      contractInitBalance = web3.eth.getBalance(contractInstance.address).toNumber();
     });
   });
 
@@ -27,20 +32,62 @@ contract ('Remittance', function(accounts) {
   });
 
   //test send transaction to contract
-  it("Should have the balance sent in initialization", function() {
+  it("Should have received funds and set passwords", function() {
     var contractBalanceFinal
     var contractBalanceNow
-    contractBalanceFinal = contractInitBalance + amount;
-    contractBalanceNow = web3.eth.getBalance(contractInstance.address).toNumber();
-    assert.equal(contractBalanceNow, contractBalanceFinal, "Contract balance is not correct.");
+    var setPassBob
+    var setPassCarol
+
+    return contractInstance.getBalance.call({from: owner})
+    .then(function(_balance) {
+      contractBalanceNow = _balance;
+    });
+
+    return contractInstance.setPass(hashedPass1, hashedPass2,{from: owner, value: amount})
+      .then(function(_result) {
+        return contractInstance.getBalance.call()
+        .then(function(_balance) {
+          contractBalanceFinal = _balance;
+        });
+        console.log(JSON.stringify("setPass returned: " + _result, null, 4));
+        assert.equal(contractBalanceNow, contractBalanceFinal, "Contract balance is not correct.");
+      });
+    //setPassBob = contractInstance.bobHashedPass.toString();
+    return contractInstance.bobHashedPass({from: owner})
+    .then(function(_result) {
+      setPassBob = _result.toString();
+      assert.equal(setPassBob, hashedPass1, "Bob's hashed password should have been passed.");
+    });
+    return contractInstance.carolHashedPass({from: owner})
+    .then(function(_result) {
+      setPassCarol = _result.toString();
+      assert.equal(setPassCarol, hashedPass2, "Carol's hashed password should have been passed.");
+    });
+
   });
 
-  //test remitFunds
-  it("Should send the contract's balance", function() {
-    return contractInstance.remitFunds(pass1,pass2,{from: owner})
+  //test withdrawFunds function
+  it("Should withdraw funds", function() {
+    var remitBalanceBefore
+    var remitBalanceNow
+
+    //How to promisify this to make it asynchronous???
+    //remitBalanceBefore = web3.eth.getBalance(remitAddress);
+    remitBalanceBefore = remitAddress.balance;
+
+    return contractInstance.setPass(hashedPass1, hashedPass2, {from: owner, value: amount})
+      .then(function(_result) {
+      });
+
+    return contractInstance.withdrawFunds(pass1, pass2, {from: remitAddress})
     .then(function(_result) {
-      assert.equal(_result[0], true, "Something went wrong with remitFunds.");
+      console.log(JSON.stringify("withdrawFunds returned: " + _result, null, 4));
     });
+
+    remitBalanceNow = remitAddress.balance;
+
+    assert.equal(remitBalanceNow, remitBalanceBefore + amount,
+      "Something went wrong with withdrawFunds()");
   });
 
 });
