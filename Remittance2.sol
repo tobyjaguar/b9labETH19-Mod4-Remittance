f1f4760fb43d45dff4751d29f9cf0f23418dac6c
Toby Jaguar <talgya@gmail.com>
2018-02-18 17:45:10 -0500

    refactor for admin

--- Remittance2.sol
+++ Remittance2.sol
@@ -1,14 +1,24 @@
+//Note for this to set correctly
+//hashed passes need to be Solidity hashes of bytes32, not strings
+//keccak256(bytes32) != keccak256(string)
 pragma solidity ^0.4.13;
 
+import "./Stoppable.sol";
 
-contract Remittance {
+
+contract Remittance is Stoppable {
 
     address public owner;
     address public carol;
     bytes32 public hashedPass;
     uint256 public expirationBlock;
+    uint256 public balance;
 
     bool public isSet;
+
+    event LogSetRemittance(address carolAddress, uint256 expirationBlock, uint256 amount);
+    event LogWithdraw(address sender, uint256 amount);
+    event LogRefund(address sender, uint256 balance);
 
     function Remittance()
     public
@@ -19,17 +29,19 @@ contract Remittance {
     function getBalance()
     public
     constant
+    onlyIfRunning
     returns (uint256 _balance)
     {
-        return this.balance;
+        return balance;
     }
 
     function setPass(bytes32 hashedPass1, bytes32 hashedPass2, address carolAddress, uint256 duration)
     public
     payable
+    onlyOwner
+    onlyIfRunning
     returns (bool success)
     {
-        require(owner == msg.sender);
         require(msg.value > 0);
         require(!isSet);
         require(carolAddress != 0);
@@ -40,32 +52,41 @@ contract Remittance {
         carol = carolAddress;
         expirationBlock = duration + block.number;
         isSet = true;
+        balance += msg.value;
+
+        LogSetRemittance(carolAddress, expirationBlock, msg.value);
         return true;
     }
 
     function withdrawFunds(bytes32 pass1, bytes32 pass2)
     public
+    onlyIfRunning
     returns(bool success)
     {
-        require(this.balance > 0);
+        require(balance > 0);
         require(msg.sender == carol);
         require(isSet);
         require(block.number < expirationBlock);
         require(hashedPass == keccak256(keccak256(pass1), keccak256(pass2)));
         isSet = false;
-        carol.transfer(this.balance);
+        carol.transfer(balance);
+
+        LogWithdraw(msg.sender, balance);
         return true;
     }
 
     function refund()
     public
+    onlyOwner
+    onlyIfRunning
     returns(bool success)
     {
-        require(msg.sender == owner);
         require(isSet);
         require(expirationBlock <= block.number);
         isSet = false;
-        owner.transfer(this.balance);
+        owner.transfer(balance);
+
+        LogRefund(msg.sender, balance);
         return true;
     }
 
